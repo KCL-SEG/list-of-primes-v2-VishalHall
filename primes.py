346f42d1f560f9772079de94d496faa02ead3a34
Vishal <vishal.hall@kcl.ac.uk>
2022-09-26 20:29:56 +0100

    Add statement to throw exception if value < 1

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("number_of_primes must be positive")
     list = []
+    count = 2
+    while True:
+        if len(list) == number_of_primes:
+            break
+        
+        isPrime = True # Assume number is prime
+        for i in range(2, count):
+            if count % i == 0:
+                isPrime = False
+        
+        if isPrime:
+            list += [count]
+        count += 1
+
     return list
